e00ba9f6ee28e743434b29aa46a49ed97f2cc1cc
Charles-Ay <60751378+Charles-Ay@users.noreply.github.com>
2020-03-04 16:18:52 -0500

    Done

--- Part 6/Program.cs
+++ Part 6/Program.cs
@@ -20,8 +20,10 @@ namespace Part_6
             int maxRand;
             int minRand;
             int rand;
+            int oddTotal = 0;
+            
 
-            /*
+            
             Console.WriteLine("Enter a minimum number: ");
             min = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a maximum number: ");
@@ -35,46 +37,48 @@ namespace Part_6
 
             }
             Console.WriteLine("Number is between min and max");
-            */
+            
 
-            //Console.WriteLine("Enter a score %");
-            //while (Console.ReadKey().Key != ConsoleKey.Escape)
+            Console.WriteLine("Enter test scores %'s to see how many people passed. press any button to countinue");
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
             {
-                /*Console.WriteLine("Enter a score %");
+                Console.WriteLine("Enter a score %");
                 score = Convert.ToInt32(Console.ReadLine());
                 {
                     if (score >= 70)
                     {
                         passes = passes + 1;
                         Console.WriteLine($"There are {passes} students that have passed so far and there are {fail} students that have failed so far");
-                      
+                        Console.WriteLine("Press escape to stop entering scores");
                     }
                     else
                     {
                         fail = fail + 1;
                         Console.WriteLine($"There are {passes} students that have passed so far and there are {fail} students that have failed so far");
+                        Console.WriteLine("Press escape to stop entering scores press any other button to countinue");
                     }
                 }
-          */}
-            //Console.WriteLine("Enter a odd number: ");
-            //odd = Convert.ToInt32(Console.ReadLine());
-            //for (int i = 1; i < odd; i += 2) ;
-            //{
-
-            //  Console.WriteLine("hi");
-            //}
-            //need help to do odd sum
+            }
+            Console.WriteLine("Enter a number to get a the sum of the odd numbers between 1 and the number: ");
+            odd = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= odd; i += 2)
+            {
+                oddTotal = oddTotal + i;
+                Console.WriteLine(oddTotal);
+            }
+            
 
             Console.WriteLine("Enter a minimum number");
             minRand = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a maximum number");
             maxRand = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"25 random numbeers between the minimum and maximum numbers are: ");
             for (int i = 1; i <= 25; i++)
             {
-                rand = gen.Next(minRand, maxRand + 1);
+                rand = gen.Next(minRand, maxRand);
                 Console.WriteLine(rand);
             }
-            //Only odd left 
+           
         }
     }
 }
